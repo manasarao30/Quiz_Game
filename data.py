13710802712b74dec530efa8b710e86376fd207d
Manasa Rao <54011799+manasarao30@users.noreply.github.com>
2023-03-26 06:41:31 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,103 @@
+#Generated through API from Open Trivia DB
+question_data = [
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Han Solo&#039;s co-pilot and best friend, &quot;Chewbacca&quot;, is an Ewok.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004&#039;s &quot;The Aviator&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "The word &quot;Inception&quot; came from the 2010 blockbuster hit &quot;Inception&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "In the original script of &quot;The Matrix&quot;, the machines used humans as additional computing power instead of batteries.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "In the original Star Wars trilogy, David Prowse was the actor who physically portrayed Darth Vader.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "George Lucas directed the entire original Star Wars trilogy.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "&quot;Minions&quot; was released on the June 10th, 2015.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Entertainment: Film",
+"type": "boolean",
+"difficulty": "easy",
+"question": "&quot;Foodfight!&quot; earned less than $80,000 at box office.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+}
+]
